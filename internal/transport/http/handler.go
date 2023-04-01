f0bee6af2cfef9e198667c800e830a99759430b6
Means <amajakai14@gmail.com>
2023-04-01 20:43:24 +0900

    ci 4

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -28,7 +28,7 @@ func NewHandler(service CommentService) *Handler {
 	h.mapRoutes()
 
 	h.Server = &http.Server{
-		Addr:    "localhost:8080",
+		Addr:    ":8080",
 		Handler: h.Router,
 	}
 	return h
